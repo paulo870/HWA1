9ba4e6363ce1cea8761d19dd406672ffba7efd36
paulo870 <paulo@poliglotaangola.com>
2026-08-07 12:34:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -382,7 +382,7 @@ audioBtn.addEventListener("click", function(e) {
         btn.textContent = `Audio ${trackNumber}`;
 
         btn.addEventListener("click", () => {
-            const folder = bookType === "student" ? "student_book_audios" : "activity_book_audios";
+            const folder = bookType === "student" ? "student-book-audios" : "activity_book_audios";
             audioSource.src = `audios/${folder}/unit_${unit}/${track}.mp3`;
             audioPlayer.load();
             showAudioPlayer(); // <-- use the new function
